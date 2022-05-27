8cbad70e024bdd44822235c993601ea98ddbbf15
Leam Howe <48015835+leamhowe@users.noreply.github.com>
2022-05-27 09:18:24 +0100

    Adapted polar_convert code
    polar_convert package contains Python functions for converting polar stereographic coordinates.
    
    Adapted from: https://github.com/nsidc/polarstereo-lonlat-convert-py.
    
    Original software was developed by the NASA National Snow and Ice Data Center Distributed Active Archive Center. Author: Chris Torrence, September 2019
    
    polar_lonlat_to_xy() function was adapted for the northern hemisphere with a central meridian of -45 degrees to create a new function polar_lonlat_north_to_xy().

---
+++ polarstereo-lonlat-convert-py/polar_convert/__init__.py
@@ -0,0 +1,7 @@
+from polar_convert.polar_convert import (  # noqa: F401
+    polar_lonlat_to_ij,
+    polar_lonlat_to_xy,
+    polar_ij_to_lonlat,
+    polar_xy_to_lonlat,
+    polar_lonlat_north_to_xy
+)
---
+++ polarstereo-lonlat-convert-py/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='polarstereo-lonlat-convert-py',
+    version='1.0.0',
+    description=(
+        'Python functions for converting polar stereographic coordinates.'
+    ),
+    url='https://github.com/nsidc/polarstereo-lonlat-convert-py',
+    author='NSIDC Development Team',
+    packages=['polar_convert'],
+    license='MIT',
+    python_requires='>=3.6, <4.0',
+)
